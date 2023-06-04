f91c689f8e292c2c2b5a9f8a92688ed6a8501edd
unknown <jefinprajan@gmail.com>
2023-06-05 00:44:42 +0530

    first commit

---
+++ myappsql/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    contact = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    age = models.IntegerField()
